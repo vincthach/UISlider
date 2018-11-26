44af5174037120f6b9de649ed8aaa55ab780c792
Vinc Nguyen <thachnguyen@thachcomputer.local>
2018-11-26 21:50:31 +0700

    Initial Commit

---
+++ UISliderCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UISliderCode
+//
+//  Created by ThachNguyen on 11/26/18.
+//  Copyright © 2018 ThachNguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
