9e83aa5f7cb5c87e42634f7315cd87802fcea977
Thach Nguyen <pcngocthach@gmail.com>
2018-11-26 21:59:48 +0700

    Add slider by code

--- UISliderCode/ViewController.swift
+++ UISliderCode/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let slider = UISlider(frame: CGRect(x: 20, y: 50, width: 200, height: 50))
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        self.view.addSubview(slider)
     }
 
 
